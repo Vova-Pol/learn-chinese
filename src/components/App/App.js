917272dbea77b5322ff3def5ab9465bf392357af
Vova Pol <ya-chai777@yandex.ru>
2023-04-22 10:31:09 +0300

    сделал первые наброски логики компонента Quiz

---
+++ src/components/App/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Quiz from '../Quiz/Quiz';
+
+function App() {
+  return (
+    <div className="app">
+      <h1 className="app__title">Time to learn Chinese</h1>
+      <Quiz />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Quiz/Quiz.js
@@ -0,0 +1,66 @@
+import './Quiz.css';
+import { characters } from '../../data';
+import { useState } from 'react';
+
+function Quiz() {
+  const [charList, setCharList] = useState(characters);
+  const [currChar, setCurrChar] = useState(characters[0]);
+  const [rightAnswersAmount, setRightAnswersAmount] = useState(0);
+  const [wrongAnswersAmount, setWrongAnswersAmount] = useState(0);
+
+  function handleAnswer() {
+    const currCharIndex = charList.indexOf(currChar);
+    console.log(currCharIndex);
+    if (currCharIndex === 0 || currCharIndex) {
+      setCurrChar(charList[currCharIndex + 1]);
+    } else {
+      console.log('no more char');
+    }
+  }
+
+  function handleRightAnswer() {
+    handleAnswer();
+    setRightAnswersAmount(rightAnswersAmount + 1);
+  }
+
+  function handleWrongAnswer() {
+    handleAnswer();
+    setWrongAnswersAmount(wrongAnswersAmount + 1);
+  }
+
+  return (
+    <div className="quiz">
+      {currChar ? (
+        <span className="quiz__current-character">{currChar.mandarin} </span>
+      ) : (
+        <>
+          <h3>Good job!</h3>
+          <p>
+            Your result: {rightAnswersAmount}/{charList.length}
+          </p>
+        </>
+      )}
+
+      {currChar ? (
+        <div className="quiz__answers-constainer">
+          {charList.map((char) => {
+            return (
+              <button
+                key={char.id}
+                onClick={
+                  char.translation === currChar.translation
+                    ? handleRightAnswer
+                    : handleWrongAnswer
+                }
+              >
+                {char.translation}
+              </button>
+            );
+          })}
+        </div>
+      ) : null}
+    </div>
+  );
+}
+
+export default Quiz;
