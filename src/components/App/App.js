803297f387f514889c43857db82bbb44c7789392
Vova Pol <ya-chai777@yandex.ru>
2023-04-23 09:27:31 +0300

    добавил палитры на главную

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,14 +1,19 @@
 import './App.css';
 import Quiz from '../Quiz/Quiz';
 import LevelsList from '../LevelsList/LevelsList';
+import ExercisesList from '../ExercisesList/ExercisesList';
 
 function App() {
   return (
     <div className="app">
-      <div className="pal1"></div>
+      <div className="pal pal1"></div>
+      <div className="pal pal2"></div>
+      <div className="pal pal3"></div>
+
       <h1 className="app__title">Time to learn Chinese</h1>
       {/* <Quiz /> */}
-      <LevelsList />
+      {/* <LevelsList /> */}
+      <ExercisesList />
     </div>
   );
 }
