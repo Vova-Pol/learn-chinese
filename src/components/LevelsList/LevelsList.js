1181d2130352246b4adb0d41481b039036a3c9a6
Vova Pol <ya-chai777@yandex.ru>
2023-04-23 09:28:35 +0300

    переписал отрисовку компонентов с задачей бэкграунда с использованием утилиты

--- src/components/LevelsList/LevelsList.js
+++ src/components/LevelsList/LevelsList.js
@@ -1,15 +1,23 @@
-import LevelsItem from '../LevelsItem/LevelsItem';
 import './LevelsList.css';
+import LevelsItem from '../LevelsItem/LevelsItem';
+import { listItemsColors } from '../../data/colors';
+import { setBackground } from '../../utils/utils';
 
 function LevelsList() {
-  const levels = [1, 2, 3];
+  const levels = [1, 2, 3, 4, 5];
 
   return (
     <div className="levels-list">
       <h2 className="levels-list__title">Choose your level</h2>
       <ul className="levels-list__list">
         {levels.map((stage) => {
-          return <LevelsItem stage={stage} key={stage} />;
+          return (
+            <LevelsItem
+              stage={stage}
+              key={stage}
+              background={setBackground(levels.indexOf(stage), listItemsColors)}
+            />
+          );
         })}
       </ul>
     </div>
