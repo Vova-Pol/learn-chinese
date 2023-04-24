1d70b8dad8336247db8486db3f05c92cd73143d7
Vova Pol <ya-chai777@yandex.ru>
2023-04-24 15:55:20 +0300

    поправил верстку App

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,21 +4,20 @@ import LevelsList from '../LevelsList/LevelsList';
 import ExercisesList from '../ExercisesList/ExercisesList';
 import { Routes, Route } from 'react-router-dom';
 import Search from '../Search/Search';
+import Header from '../Header/Header';
 
 function App() {
   return (
     <div className="app">
-      <div className="pal pal1"></div>
-      <div className="pal pal2"></div>
-      <div className="pal pal3"></div>
-
-      <h1 className="app__title">Time to learn Chinese</h1>
-      <Routes>
-        <Route path="/levels" element={<LevelsList />} />
-        <Route path="/levels/:level" element={<ExercisesList />} />
-        <Route path="/quiz" element={<Quiz />} />
-        <Route path="/search" element={<Search />} />
-      </Routes>
+      <Header />
+      <main>
+        <Routes>
+          <Route path="/levels" element={<LevelsList />} />
+          <Route path="/levels/:level" element={<ExercisesList />} />
+          <Route path="/quiz" element={<Quiz />} />
+          <Route path="/search" element={<Search />} />
+        </Routes>
+      </main>
     </div>
   );
 }
