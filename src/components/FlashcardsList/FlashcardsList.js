27b45d12ca4624f5eedc0938cd4ac39ca822adb0
Vova Pol <ya-chai777@yandex.ru>
2023-04-28 13:25:01 +0300

    обернул карточки в Линк с ссылкой на БКРС

--- src/components/FlashcardsList/FlashcardsList.js
+++ src/components/FlashcardsList/FlashcardsList.js
@@ -1,7 +1,8 @@
 import './FlashcardsList.css';
 import { api } from '../../utils/Api';
 import { useState, useEffect } from 'react';
-import { useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
+import { BKRS_SERACH_URL } from '../../utils/appConfig';
 
 function FlashcardsList() {
   const [flashcardsList, setFlashcardsList] = useState([]);
@@ -24,13 +25,22 @@ function FlashcardsList() {
         if (card.origin === origin && card.episode === episode) {
           return (
             <li className="flashcards-list__item" key={card._id}>
-              <h3 className="flashcards-list__item-title">{card.character}</h3>
-              <div className="flashcards-list__item-info">
-                <p className="flashcards-list__item-pinyin">{card.pinyin}</p>
-                <p className="flashcards-list__item-translation">
-                  {card.translation}
-                </p>
-              </div>
+              <Link
+                to={BKRS_SERACH_URL + card.character}
+                className="flashcards-list__link"
+                target="_blank"
+              >
+                <h3 className="flashcards-list__item-title">
+                  {card.character}
+                </h3>
+                <div className="flashcards-list__item-info">
+                  <p className="flashcards-list__item-pinyin">{card.pinyin}</p>
+                  <p className="flashcards-list__item-translation">
+                    {card.translation}
+                  </p>
+                </div>
+                <span className="flashcards-list__item-edit-button"></span>
+              </Link>
             </li>
           );
         } else {
