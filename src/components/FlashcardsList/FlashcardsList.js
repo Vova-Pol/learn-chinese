63c35f18e9e08ff9e38c7a1a643d3df16f5e6c01
Vova Pol <ya-chai777@yandex.ru>
2023-04-28 14:03:25 +0300

    написал кнопки Study и Quiz в списке карточек

--- src/components/FlashcardsList/FlashcardsList.js
+++ src/components/FlashcardsList/FlashcardsList.js
@@ -20,34 +20,56 @@ function FlashcardsList() {
       });
   }, []);
   return (
-    <ul className="flashcards-list">
-      {flashcardsList.map((card) => {
-        if (card.origin === origin && card.episode === episode) {
-          return (
-            <li className="flashcards-list__item" key={card._id}>
-              <Link
-                to={BKRS_SERACH_URL + card.character}
-                className="flashcards-list__link"
-                target="_blank"
-              >
-                <h3 className="flashcards-list__item-title">
-                  {card.character}
-                </h3>
-                <div className="flashcards-list__item-info">
-                  <p className="flashcards-list__item-pinyin">{card.pinyin}</p>
-                  <p className="flashcards-list__item-translation">
-                    {card.translation}
-                  </p>
-                </div>
-                <span className="flashcards-list__item-edit-button"></span>
-              </Link>
-            </li>
-          );
-        } else {
-          return;
-        }
-      })}
-    </ul>
+    <div className="flashcards-list">
+      <h1 className="flashcards-list__title">Flashcards</h1>
+      <h2 className="flashcards-list__subtitle">
+        {origin} • {episode}
+      </h2>
+      <div className="flashcards-list__links-container">
+        <Link
+          to={`/flashcards/${origin}/${episode}/study`}
+          className="flashcards-list__study-link"
+        >
+          Study
+        </Link>
+        <Link
+          to={`/flashcards/${origin}/${episode}/quiz`}
+          className="flashcards-list__quiz-link"
+        >
+          Quiz
+        </Link>
+      </div>
+      <ul className="flashcards-list__list">
+        {flashcardsList.map((card) => {
+          if (card.origin === origin && card.episode === episode) {
+            return (
+              <li className="flashcards-list__item" key={card._id}>
+                <Link
+                  to={BKRS_SERACH_URL + card.character}
+                  className="flashcards-list__item-link"
+                  target="_blank"
+                >
+                  <h3 className="flashcards-list__item-title">
+                    {card.character}
+                  </h3>
+                  <div className="flashcards-list__item-info">
+                    <p className="flashcards-list__item-pinyin">
+                      {card.pinyin}
+                    </p>
+                    <p className="flashcards-list__item-translation">
+                      {card.translation}
+                    </p>
+                  </div>
+                  <span className="flashcards-list__item-edit-button"></span>
+                </Link>
+              </li>
+            );
+          } else {
+            return;
+          }
+        })}
+      </ul>
+    </div>
   );
 }
 
