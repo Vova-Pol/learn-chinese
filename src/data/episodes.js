5dfda34f82aaa04ed0d9ba34f2f98f1a008ce75a
Vova Pol <ya-chai777@yandex.ru>
2023-05-01 11:34:14 +0300

    создал черновик данных для ORIGINS

---
+++ src/data/episodes.js
@@ -0,0 +1,43 @@
+const chineseClasses101Episodes = {
+  ['摆放面包']: {
+    origin: 'ChineseClass101.com',
+    videoId: 'X3sI3PVkgmI',
+    startTime: '0:18',
+    endTime: '2:58',
+  },
+};
+
+const littleReunionEpisodes = {
+  1: {
+    origin: '小欢喜',
+    videoId: 'mTMb8e_gDzE',
+    startTime: '1:41',
+    endTime: '3:15',
+  },
+
+  2: {
+    origin: '小欢喜',
+    videoId: 'mTMb8e_gDzE',
+    startTime: '1:41',
+    endTime: '3:15',
+  },
+
+  3: {
+    origin: '小欢喜',
+    videoId: 'mTMb8e_gDzE',
+    startTime: '1:41',
+    endTime: '3:15',
+  },
+};
+
+const littleReunion = {
+  title: '小欢喜',
+  episodes: littleReunionEpisodes,
+};
+
+const chineseClass101 = {
+  title: 'ChineseClass101.com',
+  episodes: chineseClasses101Episodes,
+};
+
+export const ORIGINS_DATA = [littleReunion, chineseClass101];
