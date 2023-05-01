38d27e3f33096a967c83bdbcdf93df1be826faee
Vova Pol <ya-chai777@yandex.ru>
2023-05-01 11:34:43 +0300

    написал предворительную логику отображения видео

--- src/components/VideoOrigin/VideoOrigin.js
+++ src/components/VideoOrigin/VideoOrigin.js
@@ -1,10 +1,40 @@
 import { useState } from 'react';
 import './VideoOrigin.css';
 import { renderYouTubeLink } from '../../utils/utils';
+import { useParams } from 'react-router-dom';
+import { ORIGINS_DATA } from '../../data/episodes';
 
 function VideoOrigin() {
   const [replay, setReplay] = useState(false);
   const [playerSpeed, setPlayerSpeed] = useState(1);
+
+  const { origin, episode } = useParams();
+
+  // Кривой способ получения данных для ссылки на видео
+  // Фикс: исправить данные карточек и данные ORIGINS_DATA так,
+  // чтобы они имели связь (занести ORIGINS в БД)
+
+  const getVideoData = (origin, episodeTitle) => {
+    let videoId;
+    let startTime;
+    let endTime;
+    if (origin === '小欢喜') {
+      const episode = ORIGINS_DATA[0].episodes.episodeTitle;
+      videoId = episode.videoId;
+      startTime = episode.startTime;
+      endTime = episode.endTime;
+      return { videoId, startTime, endTime };
+    } else if (origin === 'ChineseClass101.com') {
+      const episode = ORIGINS_DATA[1].episodes[episodeTitle];
+      videoId = episode.videoId;
+      startTime = episode.startTime;
+      endTime = episode.endTime;
+      return { videoId, startTime, endTime };
+    }
+  };
+
+  const { videoId, startTime, endTime } = getVideoData(origin, episode);
+  console.log(videoId, startTime);
 
   return (
     <div className="video-origin">
@@ -14,7 +44,7 @@ function VideoOrigin() {
         type="text/html"
         width="480"
         height="270"
-        src={renderYouTubeLink('mTMb8e_gDzE', '1:41', '3:15')}
+        src={renderYouTubeLink(videoId, startTime, endTime)}
         allowFullScreen
       ></iframe>
       <div className="video-origin__player-buttons">
