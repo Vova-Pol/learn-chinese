2b01b7600ead29d2681cc1017c52d500b48cfb6f
Vova Pol <ya-chai777@yandex.ru>
2023-05-02 19:45:07 +0300

    доработал верстку компонента EpisodesByOrigin

--- src/components/EpisodesByOrigin/EpisodesByOrigin.js
+++ src/components/EpisodesByOrigin/EpisodesByOrigin.js
@@ -1,24 +1,43 @@
 import { Link } from 'react-router-dom';
 import './EpisodesByOrigin.css';
+import thumbnailLittleReunion from '../../images/little-reunion-thumbnail.png';
+import thumbnailChineseClass101 from '../../images/chinese-class-101.png';
 
 function EpisodesByOrigin(props) {
+  // Кривоватое решение для импорта thumbnail
+  // Идея с require() почему-то не сработала
+  const thumbnail =
+    props.originTitle === '小欢喜'
+      ? thumbnailLittleReunion
+      : props.originTitle === 'ChineseClass101.com'
+      ? thumbnailChineseClass101
+      : null;
+
   return (
     <div className="episodes-by-origin">
       <h2 className="episodes-by-origin__title">{props.originTitle}</h2>
-      <ul className="episodes-by-origin__list">
-        {props.episodes.map((episode, i) => {
-          return (
-            <li key={i} className="episodes-by-origin__item">
-              <Link
-                className="episodes-by-origin__link"
-                to={`/flashcards/${props.originTitle}/${episode.title}`}
-              >
-                {episode.title}
-              </Link>
-            </li>
-          );
-        })}
-      </ul>
+      <div className="episodes-by-origin__info-container">
+        <img
+          // src={require(props.thumbnailPath)}
+          src={thumbnail}
+          alt={props.title}
+          className="episodes-by-origin__thumbnail"
+        ></img>
+        <ul className="episodes-by-origin__list">
+          {props.episodes.map((episode, i) => {
+            return (
+              <li key={i} className="episodes-by-origin__item">
+                <Link
+                  className="episodes-by-origin__link"
+                  to={`/flashcards/${props.originTitle}/${episode.title}`}
+                >
+                  {episode.title}
+                </Link>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
     </div>
   );
 }
