6a51df1abe31d3ca048b129b106cffc0b697be69
Vova Pol <ya-chai777@yandex.ru>
2023-05-04 17:38:34 +0300

    удалил ненужные поля из файла с датой

--- src/data/origins.js
+++ src/data/origins.js
@@ -1,78 +1,44 @@
 const chineseClasses101Episodes = [
   {
     title: '摆放面包',
-
-    videoId: 'X3sI3PVkgmI',
-    startTime: '0:18',
-    endTime: '2:58',
   },
 ];
 
 const littleReunionEpisodes = [
   {
     title: '1',
-    videoId: 'mTMb8e_gDzE',
-    startTime: '1:41',
-    endTime: '3:15',
   },
 
   {
     title: '2',
-    videoId: 'mTMb8e_gDzE',
-    startTime: '3:15',
-    endTime: '3:55',
   },
 
   {
     title: '3',
-    videoId: 'mTMb8e_gDzE',
-    startTime: '3:55',
-    endTime: '4:42',
   },
 
   {
     title: '4',
-    videoId: 'mTMb8e_gDzE',
-    startTime: '4:42',
-    endTime: '5:13',
   },
 
   {
     title: '5',
-    videoId: 'mTMb8e_gDzE',
-    startTime: '5:13',
-    endTime: '5:56',
   },
 
   {
     title: '6',
-    videoId: 'mTMb8e_gDzE',
-    startTime: '5:56',
-    endTime: '6:56',
   },
   {
     title: '7',
-    videoId: 'mTMb8e_gDzE',
-    startTime: '6:57',
-    endTime: '7:52',
   },
   {
     title: '8',
-    videoId: 'mTMb8e_gDzE',
-    startTime: '7:52',
-    endTime: '8:31',
   },
   {
     title: '9',
-    videoId: 'mTMb8e_gDzE',
-    startTime: '9:00',
-    endTime: '9:28',
   },
   {
     title: '10',
-    videoId: 'mTMb8e_gDzE',
-    startTime: '9:30',
-    endTime: '10:24',
   },
 ];
 
