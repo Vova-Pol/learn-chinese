4f850024f38fe70b8d3017dcb0dea362a9b98199
Vova Pol <ya-chai777@yandex.ru>
2023-05-04 17:34:49 +0300

    доработал верстку CreateFlashcards

--- src/components/CreateFlashcard/CreateFlashcard.js
+++ src/components/CreateFlashcard/CreateFlashcard.js
@@ -2,15 +2,18 @@ import './CreateFlashcard.css';
 import { useFormAndValidation } from '../../hooks/useFormAndValidation';
 import { api } from '../../utils/Api';
 import { useState } from 'react';
+import { ORIGINS_DATA } from '../../data/origins';
 
-function CreateFlashcard() {
+function CreateFlashcard(props) {
+  const defaultSelectInputValue = '小欢喜';
+
   const { values, handleChange, setValues, errors, isValid, resetForm } =
     useFormAndValidation({
       character: '',
       pinyin: '',
       translation: '',
-      origin: '',
-      episode: '',
+      origin: defaultSelectInputValue,
+      episode: props.episode,
     });
 
   const [resultText, setResultText] = useState('');
@@ -26,65 +29,90 @@ function CreateFlashcard() {
         console.log(res);
         setIsSuccessful(true);
         setResultText('Карточка успешно создана!');
+        resetForm();
       })
       .catch((err) => {
         setIsSuccessful(false);
         setResultText('Произошла ошибка на сервере. Попробуйте ещё раз.');
         console.error(err);
       });
-
-    resetForm();
   }
 
   return (
     <div className="create-flashcard">
-      <h1 className="create-flashcard__title">Create a Flashcard</h1>
       <form className="create-flashcard__form" onSubmit={handleSubmit}>
+        <label htmlFor="character" className="create-flashcard__label">
+          Character:
+        </label>
         <input
           className="create-flashcard__input"
           type="text"
+          id="character"
           name="character"
           value={values.character}
           onChange={handleChange}
           required
           placeholder="你好"
         ></input>
+        <label htmlFor="pinyin" className="create-flashcard__label">
+          Pinyin:
+        </label>
         <input
           className="create-flashcard__input"
           type="text"
+          id="pinyin"
           name="pinyin"
           value={values.pinyin}
           onChange={handleChange}
           required
           placeholder="nǐ hǎo"
         ></input>
+        <label htmlFor="translation" className="create-flashcard__label">
+          Translation:
+        </label>
         <input
           className="create-flashcard__input"
           type="text"
+          id="translation"
           name="translation"
           value={values.translation}
           onChange={handleChange}
           required
           placeholder="здравствуйте, привет"
         ></input>
+        <label htmlFor="episode" className="create-flashcard__label">
+          Episode:
+        </label>
         <input
           className="create-flashcard__input"
           type="text"
-          name="origin"
-          value={values.origin}
-          onChange={handleChange}
-          required
-          placeholder="Chinese Classes 101"
-        ></input>
-        <input
-          className="create-flashcard__input"
-          type="text"
+          id="episode"
           name="episode"
           value={values.episode}
           onChange={handleChange}
           required
-          placeholder="1 / At the bakery"
         ></input>
+        <label htmlFor="origin" className="create-flashcard__label">
+          Origin:
+        </label>
+        <select
+          className="create-flashcard__input"
+          type="text"
+          id="origin"
+          name="origin"
+          onChange={handleChange}
+          defaultValue={defaultSelectInputValue}
+          required
+        >
+          {ORIGINS_DATA.map((origin, i) => {
+            return (
+              <option key={i} value={origin.title}>
+                {origin.title}
+              </option>
+            );
+          })}
+        </select>
+
         <button type="submit" className="create-flashcard__button">
           Create
         </button>
