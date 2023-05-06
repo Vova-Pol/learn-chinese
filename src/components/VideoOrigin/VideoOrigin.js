9fd8ae270152583a0c532928c7ac2885fb9f175e
Vova Pol <ya-chai777@yandex.ru>
2023-05-06 10:09:09 +0300

    удалил кнопку replay и написал логику кнопки speed в компоненте VideoOrigin

--- src/components/VideoOrigin/VideoOrigin.js
+++ src/components/VideoOrigin/VideoOrigin.js
@@ -4,22 +4,34 @@ import { useParams } from 'react-router-dom';
 import { getVideoFileLocal } from '../../utils/utils.ts';
 
 function VideoOrigin() {
-  const [replay, setReplay] = useState(false);
-  const [playerSpeed, setPlayerSpeed] = useState(1);
+  const [speed, setSpeed] = useState(80);
 
   const { origin, episode } = useParams();
+
+  function handleSpeedButton() {
+    if (speed === 80) {
+      setSpeed(100);
+    } else {
+      setSpeed(80);
+    }
+  }
 
   return (
     <div className="video-origin">
       <video
         className="video-origin__video"
-        src={getVideoFileLocal(episode)}
+        src={getVideoFileLocal(episode, speed)}
         controls
         preload="auto"
       ></video>
       <div className="video-origin__player-buttons">
-        <button type="button" className="video-origin__speed-button"></button>
-        <button type="button" className="video-origin__replay-button"></button>
+        <button
+          onClick={handleSpeedButton}
+          type="button"
+          className="video-origin__speed-button"
+        >
+          {speed === 80 ? '80%' : '100%'}
+        </button>
       </div>
     </div>
   );
