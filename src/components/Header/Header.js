c8dccb1b9b4c10661d6186855e13032a8a1fd181
Vova Pol <ya-chai777@yandex.ru>
2023-05-06 10:23:09 +0300

    добавил логотип в шапку

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,14 @@
 import { Link } from 'react-router-dom';
+import { ReactComponent as Logo } from '../../images/logo.svg';
 import './Header.css';
 
 function Header() {
   return (
     <header className="header">
       <nav className="header__navbar">
+        <Link to="/">
+          <Logo className="header__logo" />
+        </Link>
         <Link className="header__link" to="/quiz">
           Quiz
         </Link>
