93c8f63010b70777739e4b3b23a87d1b2ebe4033
Vova Pol <ya-chai777@yandex.ru>
2023-05-12 17:03:54 +0300

    начальная верстка футера

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import './Footer.css';
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <div className="footer__links-container"></div>
+      <div className="footer__author-container"></div>
+    </footer>
+  );
+}
+
+export default Footer;
